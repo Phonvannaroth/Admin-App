c8d61504b20ac2e131c5aef4bcfdd5206ceb586f
nullDE <h.delyethan123@gmail.com>
2018-12-12 22:28:34 +0700

    post job

--- src/app/interface/company.ts
+++ src/app/interface/company.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface Company {
     key?:string;
     name?:string;
@@ -11,35 +10,3 @@ export interface Company {
     address_2:string;
     date_create?:Date;
 }
-
-export interface PostJob{
-    key?:string;
-    company_key?:string;
-    category_key?:string;
-    jobtitle?:string;
-    exp:number;
-    hiring:number;
-    salary:string;
-    gender:string;
-    age:string;
-    postdate?:Date;
-    expdate?:Date;
-    term:string;
-    industry:string;
-    qualification:string;
-    language:string;
-    location?:string;
-    requirement:string;
-    description:string;
-}
-=======
-export interface Companykey {
-    key?: string;
-    CName: string;
-    email: string;
-    website: string;
-    facebook: string;
-    address1: string;
-    address2: string;
-}
->>>>>>> fab476a46a603a366fac195513e30c3477e4b99e
---
+++ src/app/interface/postjob.ts
@@ -0,0 +1,20 @@
+export interface Postjob{
+    key?:string;
+    company_key?:string;
+    category_key?:string;
+    jobtitle?:string;
+    exp:number;
+    hiring:number;
+    salary:string;
+    gender:string;
+    age:string;
+    postdate?:Date;
+    expdate?:Date;
+    term:string;
+    industry:string;
+    qualification:string;
+    language:string;
+    location?:string;
+    requirement:string;
+    description:string;
+}
